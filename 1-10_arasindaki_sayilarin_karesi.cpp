99fa18d0712ee1b593b578573175d7334bbb39bf
elmasbusenur <elmasbusenur@gmail.com>
2019-01-18 00:12:51 +0300

    Add files via upload

---
+++ 1-10_arasindaki_sayilarin_karesi.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+	/*Write C code
+to print the square of all numbers from 1 to10.*/
+
+	for(int i=1;i<=10;i++){
+		
+		int sayi=i*i;
+		printf("%d\n",sayi);		
+
+}
+return 0;
+}
+
